880f9c80766ff3accb03a72918378396a5bf60ca
dante <lkstudio.dante@gmail.com>
2022-12-17 01:00:18 +0900

    no message

---
+++ Programming/C#/Example/Solve/Classes/Solve_01/CSolve_01.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solve.Classes.Solve_01 {
+	internal class CSolve_01 {
+		/** 초기화 */
+		public static void Start(string[] args) {
+
+		}
+	}
+}
---
+++ Programming/C#/Example/Solve/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solve {
+	internal class Program {
+		static void Main(string[] args) {
+			Classes.Solve_01.CSolve_01.Start(args);
+			Console.ReadKey();
+		}
+	}
+}
