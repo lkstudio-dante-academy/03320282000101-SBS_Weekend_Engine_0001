9f7d6d001146a40c4e05dac3b6e0eaeca1f20400
dante <lkstudio.dante@gmail.com>
2023-01-06 15:24:50 +0900

    no message

--- Programming/C#/Example/Practice/Classes/Practice_01/CPractice_01.cs
+++ Programming/C#/Example/Practice/Classes/Practice_01/CPractice_01.cs
@@ -1,5 +1,5 @@
-//#define P01_01
-//#define P01_02
+#define P01_01
+#define P01_02
 #define P01_03
 
 using System;
@@ -8,52 +8,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/*
- * 과제 1 - 1
- * - 점수를 입력 받아 해당 점수에 해당하는 학점 출력하기 (단, 1 자리 수에 따라 -, 0, + 출력)
- * 
- * -, 0, + 점수 범위
- * - 0 ~ 3 : -
- * - 4 ~ 6 : 0
- * - 7 ~ 9 : +
- * 
- * Ex)
- * 점수 입력 : 65
- * 결과 : D0
- * 
- * 
- * 과제 1 - 2
- * - 입력 된 정수의 합계와 평균 출력하기 (단, 0 이하의 점수가 입력 되면 입력 종료 및 결과 출력)
- * 
- * Ex)
- * 정수 1 입력 : 1
- * 정수 2 입력 : 3
- * 정수 3 입력 : 8
- * 정수 4 입력 : 0
- * 
- * =====> 결과 <=====
- * 합계 : 12
- * 평균 : 4
- * 
- * 
- * 과제 1 - 3
- * - 라인 수를 입력 받아 해당 라인 수 만큼 * 출력하기
- * 
- * Ex)
- * 라인 수 입력 : 5
- * 
- * =====> 결과 <=====
- *     *
- *    ***
- *   *****
- *  *******
- * *********
- */
 namespace Practice.Classes.Practice_01 {
 	internal class CPractice_01 {
 		/** 초기화 */
 		public static void Start(string[] args) {
 #if P01_01
+			/*
+			 * 과제 1 - 1
+			 * - 점수를 입력 받아 해당 점수에 해당하는 학점 출력하기 (단, 1 자리 수에 따라 -, 0, + 출력)
+			 * 
+			 * -, 0, + 점수 범위
+			 * - 0 ~ 3 : -
+			 * - 4 ~ 6 : 0
+			 * - 7 ~ 9 : +
+			 * 
+			 * Ex)
+			 * 점수 입력 : 65
+			 * 결과 : D0
+			 */
 			Console.Write("점수 입력 : ");
 			int nScore = int.Parse(Console.ReadLine());
 
@@ -79,6 +51,20 @@ namespace Practice.Classes.Practice_01 {
 				}
 			}
 #elif P01_02
+			/*
+			 * 과제 1 - 2
+			 * - 입력 된 정수의 합계와 평균 출력하기 (단, 0 이하의 점수가 입력 되면 입력 종료 및 결과 출력)
+			 * 
+			 * Ex)
+			 * 정수 1 입력 : 1
+			 * 정수 2 입력 : 3
+			 * 정수 3 입력 : 8
+			 * 정수 4 입력 : 0
+			 * 
+			 * =====> 결과 <=====
+			 * 합계 : 12
+			 * 평균 : 4
+			 */
 			int nVal = 0;
 			int nNumVals = 0;
 
@@ -115,6 +101,20 @@ namespace Practice.Classes.Practice_01 {
 			Console.WriteLine("합계 : {0}", nSumVal);
 			Console.WriteLine("평균 : {0}", nSumVal / (float)nNumVals);
 #elif P01_03
+			/*
+			 * 과제 1 - 3
+			 * - 라인 수를 입력 받아 해당 라인 수 만큼 * 출력하기
+			 * 
+			 * Ex)
+			 * 라인 수 입력 : 5
+			 * 
+			 * =====> 결과 <=====
+			 *     *
+			 *    ***
+			 *   *****
+			 *  *******
+			 * *********
+			 */
 			Console.Write("라인 수 입력 : ");
 			int nNumLines = int.Parse(Console.ReadLine());
 
